d7e3687b507cdb27a172e52888d3089c8dbc9452
Hemant singh <capscomtech@gmail.com>
2021-08-04 09:31:18 +0530

    register route added

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const PORT=2000;
 app.get("/", (req, res)=>{
     res.send("welcome");
 })
+app.get("/register", (req, res)=>{
+    res.send("This is register page.");
+})
 
 app.listen(PORT, ()=>{
     console.log("Server running");
