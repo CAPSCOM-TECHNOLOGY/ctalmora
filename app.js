67635012da2d132d0befce60b768b1a023cb68c6
Hemant singh <capscomtech@gmail.com>
2021-08-04 09:53:48 +0530

    register login view created and asset public folder created

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 let express=require("express");
 let app=express();
+let path=require("path");
 const PORT=2000;
+app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine","ejs");
+
 app.get("/", (req, res)=>{
-    res.send("welcome");
+    res.render("home");
 })
 app.get("/register", (req, res)=>{
-    res.send("This is register page.");
+    res.render("register");
+})
+app.get("/login", (req, res)=>{
+    res.render("login");
 })
 
 app.listen(PORT, ()=>{
